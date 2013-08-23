0351a1a42a0084788b34188696f821a1c27e1589
TeeYum <tnt5027@gmail.com>
2013-08-23 17:06:41 -0400

    Updated arrays and created template array 0

--- challenge_data.js
+++ challenge_data.js
@@ -1,45 +1,32 @@
       //     
-	  var challengeNumber = 2;
+	  var challengeNumber = 1;
 	 
 	  /* Challenge Variables */ 
       var challenge = [
 	    //Start Copying the Array Here
         {challengetype: "structured",  //options are "freeform", "structured"
-         question:"Do you eat your peas with honey?",
+         question:"Your question text goes here",
          media:"image", //options are "image", "video", or "none" 
          videosource:"//www.youtube.com/embed/LSFX9vrwJf8",
-         imagesource:"http://i4.mirror.co.uk/incoming/article1443682.ece/ALTERNATES/s615/Rowan%20Atkinson%20as%20Mr%20Bean-1443682",
-         imagedescription:"Mr. Bean",
-         answers:["always", "sometimes", "never"],
-         answertags:["always", "sometimes", "never"],
-         challengetag:"etstest2",
-         primarytag:"etstest"},
+         imagesource:"http://i4.mirror.co.uk/incoming/article1443682.ece/ALTERNATES/s615/Rowan%20Atkinson%20as%20Mr%20Bean-1443682", //image URLs go here
+         imagedescription:"Mr. Bean", //this is the info that will accompany any photo
+         answers:["always", "sometimes", "never"],//these are your answer options - you can name and call them anything
+         answertags:["always", "sometimes", "never"],//these are the tags that accompany the answer choice IF structured question is selected
+         challengetag:"etstest2",//tag associated with the challenge
+         primarytag:"soc119"},//this should always be soc119
 		 //Stop Copying the Array Here
-
+    
         //Start Copying the Array Here
         {challengetype: "freeform",  //options are "freeform", "structured"
-         question:"Bugatti Veyron or McLaren P1?",
+         question:"Pose with someone of a different race or culture who you do NOT know and make the photo look like s/he is your friend or family. Watch the following video for inspiration:",
          media:"video", //options are "image", "video", or "none" 
-         videosource:"//www.youtube.com/embed/LSFX9vrwJf8",
-         imagesource:"http://i4.mirror.co.uk/incoming/article1443682.ece/ALTERNATES/s615/Rowan%20Atkinson%20as%20Mr%20Bean-1443682",
-         imagedescription:"Mr. Bean",
-         answers:["always", "sometimes", "never"],
-         answertags:["always", "sometimes", "never"],
-         challengetag:"etstest2",
-         primarytag:"etstest"},
-         //Stop Copying the Array Here
-
-        //Start Copying the Array Here
-        {challengetype: "structured",  //options are "freeform", "structured"
-         question:"Bugatti Veyron or McLaren P1?",
-         media:"none", //options are "image", "video", or "none" 
-         videosource:"//www.youtube.com/embed/LSFX9vrwJf8",
+         videosource:"//www.youtube.com/embed/SELDTUaHRxQ",
          imagesource:"http://i4.mirror.co.uk/incoming/article1443682.ece/ALTERNATES/s615/Rowan%20Atkinson%20as%20Mr%20Bean-1443682",
          imagedescription:"Mr. Bean",
          answers:["always", "sometimes", "never", "always", "sometimes", "never"],
          answertags:["always", "sometimes", "never", "always", "sometimes", "never"],
-         challengetag:"etstest2",
-         primarytag:"etstest"},
+         challengetag:"ycst001",
+         primarytag:"soc119"},
          //Stop Copying the Array Here
 
 
