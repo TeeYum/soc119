ca3ad9cecac05ad310c0d0f59bb6eedd73d2a32a
TeeYum <tnt5027@gmail.com>
2013-08-28 15:49:00 -0400

    added challenge 2

--- challenge_data.js
+++ challenge_data.js
@@ -4,11 +4,11 @@
 	  /* Challenge Variables */ 
       var challenge = [
 	    //Start Copying the Array Here
-        {challengetype: "structured",  //options are "freeform", "structured"
+        {challengetype: "optionhere",  // options are "freeform", "structured"
          question:"Your question text goes here",
-         media:"image", //options are "image", "video", or "none" 
-         videosource:"//www.youtube.com/embed/LSFX9vrwJf8",
-         imagesource:"http://i4.mirror.co.uk/incoming/article1443682.ece/ALTERNATES/s615/Rowan%20Atkinson%20as%20Mr%20Bean-1443682", //image URLs go here
+         media:"mediaoptionhere", //options are "image", "video", or "none" 
+         videosource:"//www.yourvideoembedlinkhere.com",
+         imagesource:"http://yourimgurlhere.com", //image URLs go here
          imagedescription:"Mr. Bean", //this is the info that will accompany any photo
          answers:["always", "sometimes", "never"],//these are your answer options - you can name and call them anything
          answertags:["always", "sometimes", "never"],//these are the tags that accompany the answer choice IF structured question is selected
@@ -28,6 +28,19 @@
          challengetag:"ycst001",
          primarytag:"soc119"},
          //Stop Copying the Array Here
-
-
+         
+         //Start Copying the Array Here
+        {challengetype: "structured",
+         question:"President Obama needs to speak about race relations more often. Respond and tell us what's behind your response.",
+         media:"image", //options are "image", "video", or "none" 
+         videosource:"//www.youtube.com/embed/SELDTUaHRxQ",
+         imagesource:"http://graphics8.nytimes.com/images/2013/08/28/us/OBAMA-1/OBAMA-1-articleLarge.jpg",
+         imagedescription:"President, Not Preacher, but Speaking More on Race",
+         answers:["agree", "disagree"],
+         answertags:["agree", "disagree"],
+         challengetag:"ycst002",
+         primarytag:"soc119"},
+         //Stop Copying the Array Here
+         
+         
        ];
